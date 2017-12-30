6f94536ab192f3d59400da80730520fe78d139a2
Haroon Rashid <haroonr@iiitd.ac.in>
2017-12-30 22:33:49 +0000

    final script for co and fhmm on seconds data

--- nilmtk_pycharm/compare_dissag_error_seconds_data.py
+++ nilmtk_pycharm/compare_dissag_error_seconds_data.py
@@ -16,7 +16,7 @@ np.random.seed(123)
 #%%
 # List all houses in a directory
 dir = "/Volumes/MacintoshHD2/Users/haroonr/Detailed_datasets/Dataport/mix_homes/default/injected_anomalies/"
-#savedir = "/Volumes/MacintoshHD2/Users/haroonr/Dropbox/nilmtk_work/disagg_results/"
+savedir = "/Volumes/MacintoshHD2/Users/haroonr/Dropbox/nilmtk_work/disagg_results/"
 execfile("/Volumes/MacintoshHD2/Users/haroonr/Dropbox/nilmtk_work/nilmtk_pycharm/localize_fhmm.py")
 execfile("/Volumes/MacintoshHD2/Users/haroonr/Dropbox/nilmtk_work/nilmtk_pycharm/localize_appliance_support.py")
 #execfile("/Volumes/MacintoshHD2/Users/haroonr/Dropbox/nilmtk_work/plot_functions.py")
@@ -24,9 +24,9 @@ execfile("/Volumes/MacintoshHD2/Users/haroonr/Dropbox/nilmtk_work/nilmtk_pycharm
 execfile("/Volumes/MacintoshHD2/Users/haroonr/Dropbox/nilmtk_work/nilmtk_pycharm/utils.py")
 execfile("/Volumes/MacintoshHD2/Users/haroonr/Dropbox/nilmtk_work/nilmtk_pycharm/co.py")
 #%%
-hos = "meter_2.csv"
+hos = "redd_home_6.csv"#"meter_2.csv"
 df = pd.read_csv(dir + hos, index_col='localminute')  
-df.index = pd.to_datetime(df.index)
+df.index = pd.to_datetime(df.index)df
 res = df.sum(axis=0)
 high_energy_apps = res.nlargest(6).keys() # CONTROL : selects few appliances
 df_new = df[high_energy_apps]
@@ -36,13 +36,13 @@ df_new['use'] = df_new.sum(axis=1) # create new aggregate column
 #%%
 
 #FOR iawe
-#train_dset = df_new.truncate(before="2013-07-13", after="2013-07-20 23:59:59")
-#test_dset = df_new.truncate(before="2013-07-21", after="2013-08-04 23:59:59")
+#train_dset = df_new.truncate(before="2013-07-13", after="2013-07-25 23:59:59")
+#test_dset = df_new.truncate(before="2013-07-26", after="2013-08-04 23:59:59")
 
 # For redd homes
 train_dset = df_new.truncate(before="2011-05-24", after="2011-05-27 23:59:59") # continuous data
 test_dset = df_new.truncate(before="2011-05-28", after="2011-06-13 23:59:59")
-#test_dset = df_new.truncate(before="2011-06-10", after="2011-06-10 23:59:59")
+
 if(test_dset.isnull().values.any()): # note I am replacing na values with direct 0
     test_dset = test_dset.fillna(0)
 
@@ -62,16 +62,14 @@ aggregate = sum(test_dset['use'])
 fhmm_kotler = diss_accu_metric_kotler_1(fhmm_result,aggregate)
 co_kotler = diss_accu_metric_kotler_1(co_result,aggregate)
 
-
 norm_fhmm = accuracy_metric_norm_error(fhmm_result)
 norm_co = accuracy_metric_norm_error(co_result)
-
+#%%
 concat_res = pd.concat([fhmm_rmse,co_rmse,norm_fhmm,norm_co],axis=1)
 concat_res.columns = ['fhmm_rmse','co_rmse','fhmm_norm','co_norm']
 concat_res = concat_res.round(2)
-concat_res.to_csv(savedir+"norm_rmse_"+hos)
+concat_res.to_csv(savedir+"norm_rmse_COFHMM"+hos)
 
 res_frame = pd.DataFrame(data={'algo':['fhmm_acc','co_acc'],'accuracy':[fhmm_kotler,co_kotler]})
-
 res_frame = res_frame.round(2)
-res_frame.to_csv(savedir+"accuracy_kolter_"+hos,index=False)
+res_frame.to_csv(savedir+"accuracy_kolter_COFHMM"+hos,index=False)
