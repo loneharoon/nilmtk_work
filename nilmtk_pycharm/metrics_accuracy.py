ce035a71ff15048c978be1514e8d3902d6f50582
Haroon Rashid <haroonr@iiitd.ac.in>
2018-01-17 19:03:42 +0000

    final version

--- nilmtk_pycharm/metrics_accuracy.py
+++ nilmtk_pycharm/metrics_accuracy.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-In this script, I compute all metrics related to disaggregation
+In this script, I compute all metrics related to computing precision, recall and f_score of  disaggregation technaques
 Created on Wed Jan 17 11:59:12 2018
 
 @author: haroonr
@@ -18,39 +18,58 @@ from __future__ import division
 #%%
 submetered_path = "/Volumes/MacintoshHD2/Users/haroonr/Detailed_datasets/Dataport/mix_homes/default/injected_anomalies/"
 nilm_path = "/Volumes/MacintoshHD2/Users/haroonr/Dropbox/nilmtk_work/inter_results/disagg_outputs/fhmm/"
-#%%
+co_path = "/Volumes/MacintoshHD2/Users/haroonr/Dropbox/nilmtk_work/inter_results/disagg_outputs/co/"
+lbm_path = "/Volumes/MacintoshHD2/Users/haroonr/Dropbox/nilmtk_work/inter_results/disagg_outputs/lbm/"
 
-homes =  "490.csv"
+#%%
+#homes = ["115.csv","490.csv","1463.csv","3538.csv","meter_2.csv",redd_home_6.csv"]
+homes =  "redd_home_6.csv"
 df = pd.read_csv(submetered_path+homes,index_col='localminute')
 df.index = pd.to_datetime(df.index)
-#high_energy_apps = ['use','air1','refrigerator1','laptop','tv','water_filter'] # for aiwe
 res = df.sum(axis=0)
-high_energy_apps = res.nlargest(6).keys() # CONTROL : selects few appliances
+#high_energy_apps = res.nlargest(6).keys() # CONTROL : selects few appliances
+#high_energy_apps = ['use','air1','refrigerator1','laptop','tv','water_filter'] # for aiwe
+high_energy_apps = ['use','air1','refrigerator1','electric_heat','stove','bathroom_gfi'] # for redd_home
 df_new = df[high_energy_apps]
 del df_new['use']# drop stale aggregate column
 df_new['use'] = df_new.sum(axis=1)# create new aggregate column
 #%%
-#FOR iawe
-#train_dset = df_new.truncate(before="2013-07-13", after="2013-07-20 23:59:59")
-#oracle_dset = df_new.truncate(before="2013-07-21", after="2013-08-04 23:59:59")
-# for dataport homes
-test_dset = df_new.truncate(before="2014-07-01", after="2014-08-30 23:59:59")
+
+#test_dset = df_new.truncate(before="2013-07-21", after="2013-08-04 23:59:59")#iawe
+#test_dset = df_new.truncate(before="2014-07-01", after="2014-08-30 23:59:59") #dataport
+test_dset = df_new.truncate(before="2011-05-28", after="2011-06-13 23:59:59") # redd
 #%% Let's read now disaggregatedd FHMM dataset of same home
-predict_df = pd.read_csv(nilm_path+homes,index_col='localminute') 
-predict_df.index = pd.to_datetime(predict_df.index)
+predict_df_fhm = pd.read_csv(nilm_path+homes,index_col='localminute') 
+predict_df_fhm.index = pd.to_datetime(predict_df_fhm.index)
+
+predict_df_co = pd.read_csv(co_path+homes,index_col='localminute') 
+predict_df_co.index = pd.to_datetime(predict_df_co.index)
+
+predict_df_lbm = pd.read_csv(lbm_path+homes,index_col='localminute') 
+predict_df_lbm.index = pd.to_datetime(predict_df_lbm.index)
+#%%
+compute_accuracy_results(test_dset,predict_df_fhm)
+compute_accuracy_results(test_dset,predict_df_co)
+#test_dset = test_dset[:"2014-08-29"] # for datport homes 
+#test_dset = test_dset["2014-07-02":"2014-08-29"] # for 3538.csv
+#drop_keys = ["inferred mains","mains",'microwave1','kitchenapp2'] # for 3538.csv
+drop_keys = ["inferred mains","mains"]
+predict_df_lbm = predict_df_lbm.drop(drop_keys,axis=1)
+compute_accuracy_results(test_dset,predict_df_lbm)
 
 #%%
-on_power_threshold = 10
-appliances  = predict_df.columns
-results =  OrderedDict()
-for app in appliances:
-    print (app)
-    actual = test_dset[app]
-    predict = predict_df[app]
-    when_on_actual = actual >= on_power_threshold
-    when_on_predict = predict >= on_power_threshold
-    #results[app] = precision_recall_fscore_support(when_on_actual,when_on_predict)
-    results[app] = compute_confusion_metrics(when_on_actual,when_on_predict)
+def compute_accuracy_results(test_dset,predict_df):
+    on_power_threshold = 10
+    appliances  = predict_df.columns
+    results =  OrderedDict()
+    for app in appliances:
+        actual = test_dset[app]
+        predict = predict_df[app]
+        when_on_actual = actual >= on_power_threshold
+        when_on_predict = predict >= on_power_threshold
+        #results[app] = precision_recall_fscore_support(when_on_actual,when_on_predict)
+        results[app] = compute_confusion_metrics(when_on_actual,when_on_predict) 
+    return(results)
 #%%
 def compute_confusion_metrics(actual,predict):
     res = OrderedDict()
